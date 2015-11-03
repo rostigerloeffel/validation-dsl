870f4e2328314779e8b6304f7898aec1ae2cef95
Robert Stelzmann <robert.stelzmann@isax.com>
2015-11-03 10:55:59 +0100

    - Fixed standalone setup to register model

--- com.isax.validation/com.isax.validation.dsl/src/com/isax/validation/dsl/DslStandaloneSetup.java
+++ com.isax.validation/com.isax.validation.dsl/src/com/isax/validation/dsl/DslStandaloneSetup.java
@@ -3,6 +3,10 @@
 */
 package com.isax.validation.dsl;
 
+import org.eclipse.emf.ecore.EPackage;
+
+import com.google.inject.Injector;
+
 /**
  * Initialization support for running Xtext languages 
  * without equinox extension registry
@@ -12,5 +16,13 @@ public class DslStandaloneSetup extends DslStandaloneSetupGenerated{
 	public static void doSetup() {
 		new DslStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
+	
+	@Override
+    public void register(Injector injector) {
+        if (!EPackage.Registry.INSTANCE.containsKey("http://www.isax.com/validation/dsl/Dsl")) {
+            EPackage.Registry.INSTANCE.put("http://www.isax.com/validation/dsl/Dsl", com.isax.validation.dsl.dsl.DslPackage.eINSTANCE);
+        }
+        super.register(injector);
+    }
 }
 
